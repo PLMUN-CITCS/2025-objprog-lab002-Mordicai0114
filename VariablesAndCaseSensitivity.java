ba705aab39a8dbc573b8544277e090a7fcff9df8
John Gilbert P. Pilapil <pilapiljohngilbert_bscs@plmun.edu.ph>
2025-02-26 07:29:11 -0700

    Update VariablesAndCaseSensitivity.java

--- VariablesAndCaseSensitivity.java
+++ VariablesAndCaseSensitivity.java
@@ -5,11 +5,11 @@ public class VariablesAndCaseSensitivity {
         double itemPrice = 29.99;
         String studentName = "Alice";
         int counter = 10;
-        int Counter = 20;
+        int CounterDifferentCase = 20;
     
     System.out.println("Student's age is: " + studentAge);
     System.out.println("Item's price is: " + itemPrice);
-    System.out.println("studentName is : " + studentName);
+    System.out.println("StudentName is : " + studentName);
     System.out.println("Counter: " + counter);
     System.out.println("Counter (Different Case): " + Counter);
     }
